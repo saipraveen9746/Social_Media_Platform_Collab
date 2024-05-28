13cb4db77c61c8de2f79443aac5eb20936ed1897
saipraveen9746 <saipraveenvinu7736@gmail.com>
2024-05-28 12:45:17 +0530

    'story-profile-view'

--- SocialMedia/story/serializers.py
+++ SocialMedia/story/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 from .models import Story
+from Account.models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [ 'username', 'image',]
 
 
 
 class StorySerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    # author = serializers.ReadOnlyField(source='author.username')
+    author = CustomUserSerializer(read_only=True)
+
 
     class Meta:
         model = Story
