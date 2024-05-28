109201289060c59d96069b25d4d0444a241953f7
saipraveen9746 <saipraveenvinu7736@gmail.com>
2024-05-28 14:36:47 +0530

    'statuscodeforstory'

--- SocialMedia/story/views.py
+++ SocialMedia/story/views.py
@@ -23,22 +23,38 @@ class StoryListCreateApiView(generics.ListCreateAPIView):
         following_users = user.following.all()
         now = timezone.now()
         return Story.objects.filter(author__in=following_users, created_at__gte=now - timedelta(hours=24))
-
+    
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            self.perform_create(serializer)
+        except Exception as e:
+            return Response({'status': 0, 'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        headers = self.get_success_headers(serializer.data)
+        return Response({'status': 1, 'message': 'Story created successfully', 'story': serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
+    
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        try:
+            serializer.save(author=self.request.user)
+        except Exception as e:
+            raise e 
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.serializer_class(queryset, many=True)
-        return Response({'status': 1, 'stories': serializer.data}, status=status.HTTP_200_OK)
+        try:
+            queryset = self.get_queryset()
+            serializer = self.serializer_class(queryset, many=True)
+            return Response({'status': 1, 'stories': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'status': 0, 'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class StoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Story.objects.all()
     serializer_class = StorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]    
-
-
 
 class DeleteStory(APIView):
     permission_classes = [IsAuthenticated]
