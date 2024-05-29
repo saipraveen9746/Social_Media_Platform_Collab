c393c0d7448dcdbb29f70052f838ce79a70d6e55
saipraveen9746 <saipraveenvinu7736@gmail.com>
2024-05-29 14:33:23 +0530

    'imagebioupdate'

--- SocialMedia/Account/serializers.py
+++ SocialMedia/Account/serializers.py
@@ -87,7 +87,7 @@ class BioUpdateSerializer(serializers.ModelSerializer):
         return value
     class Meta:
         model = CustomUser
-        fields = ['name','dob','phone_number','location']
+        fields = ['name','dob','phone_number','location','image']
 
 
 class SearchSerializer(serializers.ModelSerializer):
