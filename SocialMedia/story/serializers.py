67f282e4841500f75d1e550d465511bea3191106
saipraveen9746 <saipraveenvinu7736@gmail.com>
2024-06-03 16:07:26 +0530

    media-urlchange

--- SocialMedia/story/serializers.py
+++ SocialMedia/story/serializers.py
@@ -1,20 +1,38 @@
 from rest_framework import serializers
 from .models import Story
 from Account.models import CustomUser
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = [ 'username', 'image',]
-
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if representation.get('image'):
+            representation['image'] = representation['image'].replace('http://127.0.0.1:8000/', '')
+        return representation
 
 
 class StorySerializer(serializers.ModelSerializer):
-    # author = serializers.ReadOnlyField(source='author.username')
     author = CustomUserSerializer(read_only=True)
-
 
     class Meta:
         model = Story
         fields = ['id', 'author', 'caption', 'location', 'created_at', 'updated_at', 'image']
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        image = validated_data.get('image', None)
+        if image and isinstance(image, InMemoryUploadedFile):
+            # Extract the relative path from the file's name
+            validated_data['image'] = image.name
+        return super().create(validated_data)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if representation.get('image'):
+            representation['image'] = representation['image'].replace('http://127.0.0.1:8000/', '')
+        return representation
+
